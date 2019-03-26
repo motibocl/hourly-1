07ae52bd8184434f0882d19023d6a5ad116e9bf9
moti <moti@elector.co.il>
2019-03-26 17:45:07 +0200

    general 2

--- src/main/java/com/elector/Controllers/SampleController.java
+++ src/main/java/com/elector/Controllers/SampleController.java
@@ -10,56 +10,100 @@ import org.springframework.ui.Model;
 
 //
 
+import javax.annotation.PostConstruct;
 import java.sql.*;
+import java.util.ArrayList;
 
 @Controller
 @EnableAutoConfiguration
 
 public class SampleController {
-    static Connection myConn;
-    static int counter=0;
-     static String name=null;
-     static int employeeId;
-    @RequestMapping(value = "/getAlldata", method = RequestMethod.GET)
-    String getdata(@RequestParam String login, @RequestParam String pass) throws SQLException {
-        if(getAllData(login, pass))//if pass and phone correct
-        return "redirect:/main";
-        else
+    private static Connection myConn;
+    private static boolean loggedIn;//if logged in
+    private static int counter = 0;
+    private static String name = null;
+    private static int employeeId;
+
+    @PostConstruct
+    public void init() throws Exception {
+        myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?autoReconnect=true&useSSL=false", "root", "RAMI2018");
+    }
+
+    @RequestMapping(value = "/getAlldata", method = RequestMethod.POST)
+    public String getdata(@RequestParam String login, @RequestParam String pass) throws SQLException {
+        if (checkCredentials(login, pass)) {//if pass and phone correct
+            loggedIn = true;
+            return "redirect:/main";
+        } else
             return "Landing_page";
     }
-       @RequestMapping(value = "/getAlltext", method = RequestMethod.GET)
-    String getText(@RequestParam String text, @RequestParam String hoursWorked,@RequestParam String theDay) throws SQLException {
 
-        Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?autoReconnect=true&useSSL=false", "root", "tuRgmhuI1");
-        String sql ="insert into comments "+"(employeeId,comments)"+"values (?,?)";
+    @RequestMapping(value = "/getAlltext", method = RequestMethod.GET)
+    public String getText(@RequestParam String text, @RequestParam String hoursWorked, @RequestParam String theDay) throws SQLException {
+
+        // Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?autoReconnect=true&useSSL=false", "root", "tuRgmhuI1");
+        String sql = "insert into comments (employeeId,comments) values (?,?)";
         PreparedStatement preparedStmt = myConn.prepareStatement(sql);
-        preparedStmt.setInt (1, employeeId);
-        preparedStmt.setString (2, text);
+        preparedStmt.setInt(1, employeeId);
+        preparedStmt.setString(2, text);
         preparedStmt.execute();
 
         return "redirect:/main";
     }
-    @RequestMapping(value="/main" ,method = RequestMethod.GET)
-    public String mainp(Model model) throws Exception {
 
-        model.addAttribute("name", "Welcome back "+name);
-        return "main";
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String mainp(Model model) throws Exception {
+        try {
+            model.addAttribute("name", "Welcome back " + name);
+            if (!loggedIn)
+                return "Landing_page";
+            return "main";
+        } catch (Exception exc) {
+            return "error";
+        }
     }
-    @RequestMapping("/home" )
+
+    @RequestMapping("/home")
     public String home(Model model) throws Exception {
+        if (loggedIn)
+            return "redirect:/main";
         return "Landing_page";
     }
 
+    @RequestMapping("/logout")
+    public String logout(Model model) throws Exception {
+        loggedIn = false;
+        return "redirect:/home";
+    }
 
-    @RequestMapping("/reports" )
+    @RequestMapping("/reports")
     public String reports(Model model) throws Exception {
-        return "reports";
+        try {
+            if (!loggedIn)
+                return "Landing_page";
+            PreparedStatement myStmt = myConn.prepareStatement("select * from test2.comments WHERE test2.comments.employeeId=?  ");
+            myStmt.setInt(1, employeeId);
+            ResultSet rs = myStmt.executeQuery();
+            ArrayList<String> commentList = new ArrayList<String>();
+            while (rs.next()) {
+                commentList.add(rs.getString("comments"));
+            }
+            // model.addAttribute("lengthList",commentList.size() );
+
+            model.addAttribute("comment", commentList);
+
+            return "reports";
+        } catch (Exception exc) {
+            return "error";
+
+        }
     }
 
 
-
-    @RequestMapping("/special_report" )
+    @RequestMapping("/special_report")
     public String special_reports(Model model) throws Exception {
+        if (!loggedIn)
+            return "Landing_page";
         return "special_report";
     }
 
@@ -67,24 +111,23 @@ public class SampleController {
         SpringApplication.run(SampleController.class, args);
     }
 
+    //iliya said to add try catch final.
+    private static boolean checkCredentials(String login, String pass) throws SQLException {
 
-    private static boolean getAllData(String login, String pass) throws SQLException {
 
-        myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2?autoReconnect=true&useSSL=false", "root", "tuRgmhuI1");
-
-        PreparedStatement myStmt = myConn.prepareStatement("select * from test2.employee WHERE test2.employee.companyId='1' and test2.employee.employeePhone=? and test2.employee.employeePassword=? ");
+        PreparedStatement myStmt = myConn.prepareStatement("select * from test2.employee WHERE test2.employee.companyId='1234' and test2.employee.employeePhone=? and test2.employee.employeePassword=? ");
         myStmt.setString(1, login);
         myStmt.setString(2, pass);
 
-        ResultSet  rs =  myStmt.executeQuery();
+        ResultSet rs = myStmt.executeQuery();
 
-        if (rs.next()==true) {
+        if (rs.next()) {
             name = rs.getString("employeeName");
             employeeId = rs.getInt("employeeId");
             return true;
         }
 
-            return false;
+        return false;
 
     }
 
